ee70da89bac18d05cc1864be89118d625c17a029
Henri <henrisebille69@gmail.com>
2024-05-08 13:30:25 +0200

    cleaned the code a bit

--- src/main.cpp
+++ src/main.cpp
@@ -6,29 +6,30 @@
 /*   By: hsebille <hsebille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 15:51:50 by hsebille          #+#    #+#             */
-/*   Updated: 2024/05/07 15:51:52 by hsebille         ###   ########.fr       */
+/*   Updated: 2024/05/08 13:27:44 by hsebille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Vertical Line Example");
 
-    while (window.isOpen())
+    sf::Vertex line[] =
     {
+        sf::Vertex(sf::Vector2f(150, 0)), // Start point
+        sf::Vertex(sf::Vector2f(150, 300)) // End point (adjust Y coordinate for length)
+    };
+
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(sf::Color::Black);
+        window.draw(line, 2, sf::Lines); // Drawing the line
         window.display();
     }
 
