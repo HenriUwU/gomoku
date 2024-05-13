dc709373ff45631efa882e911457bbc9f4e84ba6
lanceleau02 <lancelot.prieure@gmail.com>
2024-05-13 15:29:43 +0200

    Ça fonctionne enfin BORDEL

--- src/Graphics/Graphics.cpp
+++ src/Graphics/Graphics.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Graphics.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hsebille <hsebille@student.42.fr>          +#+  +:+       +#+        */
+/*   By: laprieur <laprieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 13:53:24 by laprieur          #+#    #+#             */
-/*   Updated: 2024/05/13 15:11:21 by hsebille         ###   ########.fr       */
+/*   Updated: 2024/05/13 15:28:48 by laprieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 Graphics::Graphics(sf::RenderWindow &window) {
 	_windowWidth = window.getSize().x;
 	_windowHeight = window.getSize().y;
-	_cellSize = _gridSize / 18.0f;
 	_gridSize = _windowHeight - 75;
+	_cellSize = _gridSize / 18.0f;
 	_gridStartPoint = std::make_pair((_windowWidth - _windowHeight) / 2 + 35, 35);
 }
 
