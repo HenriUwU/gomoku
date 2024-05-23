2e64abb8a68db7c749f1bc749cc59755d47adaaf
hsebille <henrisebille69@gmail.com>
2024-05-23 16:03:24 +0200

    changing the mainMenu, added the grid on it but need some more changes

--- .build/src/Debug/Debug.d
+++
@@ -1,4 +0,0 @@
-.build/src/Debug/Debug.o: src/Debug/Debug.cpp include/Debug/Debug.hpp \
- include/gomoku.hpp include/Graphics/Graphics.hpp \
- include/Graphics/Menu/MainMenu.hpp include/Graphics/Game/Goban.hpp \
- include/Graphics/Game/Gameplay.hpp
--- .build/src/Graphics/Graphics.d
+++
@@ -1,4 +0,0 @@
-.build/src/Graphics/Graphics.o: src/Graphics/Graphics.cpp \
- include/gomoku.hpp include/Graphics/Graphics.hpp \
- include/Graphics/Menu/MainMenu.hpp include/Graphics/Game/Goban.hpp \
- include/Graphics/Game/Gameplay.hpp include/Debug/Debug.hpp
