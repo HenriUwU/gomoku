0f5f76d54b270b4106fe4f5682ec3bfe7a882e54
Lancelot Prieure <laprieur@1H5.42angouleme.fr>
2024-06-19 11:39:33 +0200

    MainMenu cleaning finished

--- include/Graphics/Menu/MainMenu.hpp
+++ include/Graphics/Menu/MainMenu.hpp
@@ -6,7 +6,7 @@
 /*   By: laprieur <laprieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 14:15:24 by laprieur          #+#    #+#             */
-/*   Updated: 2024/06/19 10:01:50 by laprieur         ###   ########.fr       */
+/*   Updated: 2024/06/19 11:39:01 by laprieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,16 @@ class MainMenu {
 		sf::Texture		_orangeButtonTexture;
 		sf::Texture		_redButtonTexture;
 		sf::Texture		_blueButtonTexture;
-		sf::Texture		_greenButtonHighlightedTexture;
-		sf::Texture		_orangeButtonHighlightedTexture;
-		sf::Texture		_redButtonHighlightedTexture;
-		sf::Texture		_blueButtonHighlightedTexture;
+		sf::Texture		_greenHoveredButtonTexture;
+		sf::Texture		_orangeHoveredButtonTexture;
+		sf::Texture		_redHoveredButtonTexture;
+		sf::Texture		_blueHoveredButtonTexture;
 		sf::Texture		_customIconTexture;
 		sf::Texture		_settingsIconTexture;
 		sf::Texture		_helpIconTexture;
 		sf::Sprite		_greenButtonSprite;
 		sf::Sprite		_orangeButtonSprite;
 		sf::Sprite		_redButtonSprite;
-		sf::Sprite		_greenButtonHighlightedSprite;
-		sf::Sprite		_orangeButtonHighlightedSprite;
-		sf::Sprite		_redButtonHighlightedSprite;
 		sf::Sprite		_blueButtonCustomSprite;
 		sf::Sprite		_blueButtonSettingsSprite;
 		sf::Sprite		_blueButtonHelpSprite;
