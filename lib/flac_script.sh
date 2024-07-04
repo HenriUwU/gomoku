44d9b76d5b07ef55b25ed41ad3932f7ab2113806
Lancelot Prieure <laprieur@1G5.42angouleme.fr>
2024-07-04 14:42:13 +0200

    test

--- lib/flac_script.sh
+++ lib/flac_script.sh
@@ -5,5 +5,7 @@ cd flac-1.4.3
 ./configure --prefix=$HOME/local
 make
 make install
+export LD_LIBRARY_PATH=$HOME/local/lib:$LD_LIBRARY_PATH
+killall -3 gnome-shell
 rm -rf flac-1.4.3.tar.xz
 rm -rf flac-1.4.3
