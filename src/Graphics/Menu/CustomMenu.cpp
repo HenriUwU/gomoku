0d1ee28b677472889fb2a3ad615da20230d3b8f8
Lancelot Prieure <laprieur@1F5.42angouleme.fr>
2024-07-10 13:39:40 +0200

    Replacing files and cleaning on Custom menu

--- src/Graphics/Menu/CustomMenu.cpp
+++ src/Graphics/Menu/CustomMenu.cpp
@@ -6,7 +6,7 @@
 /*   By: laprieur <laprieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 22:14:13 by hsebille          #+#    #+#             */
-/*   Updated: 2024/07/09 23:54:46 by laprieur         ###   ########.fr       */
+/*   Updated: 2024/07/10 13:38:53 by laprieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,172 +52,60 @@ void	CustomMenu::handleKeys(const sf::Event& event, const sf::RenderWindow& wind
 }
 
 void	CustomMenu::handleStonesSelection(const sf::RenderWindow& window) {
-	if (_stonesSprites[BLACK_WHITE].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (stonesColors == NOSTONESCOLORS)
-			_stoneSelectorSprite.setPosition(691 - 8, 379 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			stonesColors = BLACK_WHITE;
-			_stoneSelectorSprite.setPosition(691 - 8, 379 - 8);
+	bool			stoneHovered = false;
+	StonesColors	stoneColorsArray[8] = {BLACK_WHITE, GREEN_RED, SALMON_CORAL, PINK_FLUOYELLOW, BLACK_YELLOW, ORANGE_VIOLET, DARKGREEN_LIGHTGREEN, TURQUOISEGREEN_INDIGO};
+	for (int i = 0; i < 8; i++) {
+		if (_stonesSprites[i].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
+			stoneHovered = true;
+			if (stonesColors == NOSTONESCOLORS)
+				_stoneSelectorSprite.setPosition((690 + (i * 70)) - 6, 379 - 7);
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+				stonesColors = stoneColorsArray[i];
+				_stoneSelectorSprite.setPosition((690 + (i * 70)) - 6, 379 - 7);
+			}
+			break;
 		}
-	} else if (_stonesSprites[GREEN_RED].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (stonesColors == NOSTONESCOLORS)
-			_stoneSelectorSprite.setPosition(761 - 8, 379 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			stonesColors = GREEN_RED;
-			_stoneSelectorSprite.setPosition(761 - 8, 379 - 8);
-		}
-	} else if (_stonesSprites[SALMON_CORAL].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (stonesColors == NOSTONESCOLORS)
-			_stoneSelectorSprite.setPosition(831 - 8, 379 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			stonesColors = SALMON_CORAL;
-			_stoneSelectorSprite.setPosition(831 - 8, 379 - 8);
-		}
-	} else if (_stonesSprites[PINK_FLUOYELLOW].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (stonesColors == NOSTONESCOLORS)
-			_stoneSelectorSprite.setPosition(901 - 8, 379 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			stonesColors = PINK_FLUOYELLOW;
-			_stoneSelectorSprite.setPosition(901 - 8, 379 - 8);
-		}
-	} else if (_stonesSprites[BLACK_YELLOW].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (stonesColors == NOSTONESCOLORS)
-			_stoneSelectorSprite.setPosition(971 - 8, 379 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			stonesColors = BLACK_YELLOW;
-			_stoneSelectorSprite.setPosition(971 - 8, 379 - 8);
-		}
-	} else if (_stonesSprites[ORANGE_VIOLET].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (stonesColors == NOSTONESCOLORS)
-			_stoneSelectorSprite.setPosition(1041 - 8, 379 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			stonesColors = ORANGE_VIOLET;
-			_stoneSelectorSprite.setPosition(1041 - 8, 379 - 8);
-		}
-	} else if (_stonesSprites[DARKGREEN_LIGHTGREEN].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (stonesColors == NOSTONESCOLORS)
-			_stoneSelectorSprite.setPosition(1110 - 7, 379 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			stonesColors = DARKGREEN_LIGHTGREEN;
-			_stoneSelectorSprite.setPosition(1110 - 7, 379 - 8);
-		}
-	} else if (_stonesSprites[TURQUOISEGREEN_INDIGO].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (stonesColors == NOSTONESCOLORS)
-			_stoneSelectorSprite.setPosition(1179 - 6, 379 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			stonesColors = TURQUOISEGREEN_INDIGO;
-			_stoneSelectorSprite.setPosition(1179 - 6, 379 - 8);
-		}
-	} else if (stonesColors == NOSTONESCOLORS)
-			_stoneSelectorSprite.setPosition(-100, -100);
+	}
+	if (!stoneHovered && stonesColors == NOSTONESCOLORS)
+		_stoneSelectorSprite.setPosition(-100, -100);
 }
 
 void	CustomMenu::handleAvatarSelection(const sf::RenderWindow& window) {
-	if (_avatarsSprites[TOMMY].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (playerOneAvatar == NOAVATAR)
-			_avatarSelectorSprite.setPosition(691 - 8, 563 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			playerOneAvatar = TOMMY;
-			_avatarSelectorSprite.setPosition(691 - 8, 563 - 8);
+	bool	avatarHovered = false;
+	Avatar	avatarsArray[6] = {TOMMY, LAURE, ALEX, HERIC, MOUSSE, GUNTHER};
+	for (int i = 0; i < 6; i++) {
+		if (_avatarsSprites[i].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
+			avatarHovered = true;
+			if (playerOneAvatar == NOAVATAR)
+				_avatarSelectorSprite.setPosition((690 + (i * 93)) - 6, 563 - 7);
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+				playerOneAvatar = avatarsArray[i];
+				_avatarSelectorSprite.setPosition((690 + (i * 93)) - 6, 563 - 7);
+			}
+			break;
 		}
-	} else if (_avatarsSprites[LAURE].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (playerOneAvatar == NOAVATAR)
-			_avatarSelectorSprite.setPosition(784 - 8, 563 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			playerOneAvatar = LAURE;
-			_avatarSelectorSprite.setPosition(784 - 8, 563 - 8);
-		}
-	} else if (_avatarsSprites[ALEX].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (playerOneAvatar == NOAVATAR)
-			_avatarSelectorSprite.setPosition(877 - 8, 563 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			playerOneAvatar = ALEX;
-			_avatarSelectorSprite.setPosition(877 - 8, 563 - 8);
-		}
-	} else if (_avatarsSprites[HERIC].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (playerOneAvatar == NOAVATAR)
-			_avatarSelectorSprite.setPosition(970 - 8, 563 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			playerOneAvatar = HERIC;
-			_avatarSelectorSprite.setPosition(970 - 8, 563 - 8);
-		}
-	} else if (_avatarsSprites[MOUSSE].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (playerOneAvatar == NOAVATAR)
-			_avatarSelectorSprite.setPosition(1063 - 8, 563 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			playerOneAvatar = MOUSSE;
-			_avatarSelectorSprite.setPosition(1063 - 8, 563 - 8);
-		}
-	} else if (_avatarsSprites[GUNTHER].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (playerOneAvatar == NOAVATAR)
-			_avatarSelectorSprite.setPosition(1154 - 6, 563 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			playerOneAvatar = GUNTHER;
-			_avatarSelectorSprite.setPosition(1154 - 6, 563 - 8);
-		}
-	} else if (playerOneAvatar == NOAVATAR)
-			_avatarSelectorSprite.setPosition(-100, -100);
+	}
+	if (!avatarHovered && playerOneAvatar == NOAVATAR)
+		_avatarSelectorSprite.setPosition(-100, -100);
 }
 
 void	CustomMenu::handleBoardSelection(const sf::RenderWindow& window) {
-	if (_boardsSprites[AZURE].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (boardColor == NOBOARD)
-			_boardSelectorSprite.setPosition(691 - 8, 776 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			boardColor = AZURE;
-			_boardSelectorSprite.setPosition(691 - 8, 776 - 8);
+	bool		boardHovered = false;
+	BoardColor	boardsColorsArray[8] = {AZURE, YELLOW, RED, ORANGE, PINK, GREEN, GRAY, BLACK};
+	for (int i = 0; i < 8; i++) {
+		if (_boardsSprites[i].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
+			boardHovered = true;
+			if (boardColor == NOBOARD)
+				_boardSelectorSprite.setPosition((690 + (i * 70)) - 6, 776 - 7);
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+				boardColor = boardsColorsArray[i];
+				_boardSelectorSprite.setPosition((690 + (i * 70)) - 6, 776 - 7);
+			}
+			break;
 		}
-	} else if (_boardsSprites[YELLOW].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (boardColor == NOBOARD)
-			_boardSelectorSprite.setPosition(761 - 8, 776 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			boardColor = YELLOW;
-			_boardSelectorSprite.setPosition(761 - 8, 776 - 8);
-		}
-	} else if (_boardsSprites[RED].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (boardColor == NOBOARD)
-			_boardSelectorSprite.setPosition(831 - 8, 776 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			boardColor = RED;
-			_boardSelectorSprite.setPosition(831 - 8, 776 - 8);
-		}
-	} else if (_boardsSprites[ORANGE].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (boardColor == NOBOARD)
-			_boardSelectorSprite.setPosition(901 - 8, 776 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			boardColor = ORANGE;
-			_boardSelectorSprite.setPosition(901 - 8, 776 - 8);
-		}
-	} else if (_boardsSprites[PINK].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (boardColor == NOBOARD)
-			_boardSelectorSprite.setPosition(971 - 8, 776 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			boardColor = PINK;
-			_boardSelectorSprite.setPosition(971 - 8, 776 - 8);
-		}
-	} else if (_boardsSprites[GREEN].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (boardColor == NOBOARD)
-			_boardSelectorSprite.setPosition(1041 - 8, 776 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			boardColor = GREEN;
-			_boardSelectorSprite.setPosition(1041 - 8, 776 - 8);
-		}
-	} else if (_boardsSprites[GRAY].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (boardColor == NOBOARD)
-			_boardSelectorSprite.setPosition(1110 - 7, 776 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			boardColor = GRAY;
-			_boardSelectorSprite.setPosition(1110 - 7, 776 - 8);
-		}
-	} else if (_boardsSprites[BLACK].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
-		if (boardColor == NOBOARD)
-			_boardSelectorSprite.setPosition(1179 - 6, 776 - 8);
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-			boardColor = BLACK;
-			_boardSelectorSprite.setPosition(1179 - 6, 776 - 8);
-		}
-	} else if (boardColor == NOBOARD)
-			_boardSelectorSprite.setPosition(-100, -100);
+	}
+	if (!boardHovered && boardColor == NOBOARD)
+		_boardSelectorSprite.setPosition(-100, -100);
 }
 
 void	CustomMenu::init() {
